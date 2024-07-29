ed2d1724b5cb4576093f15ad1dd5c727f27c98c1
a. <noireground@proton.me>
2024-07-29 11:13:56 -0400

    if buttons added

--- Scenes/level_14.gd
+++ Scenes/level_14.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+#
+var panel_up = false
+var max_input = 20
+var active_buttons = [0,1,2,5,6,7]
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	global_audio.play_music_level("farm")
